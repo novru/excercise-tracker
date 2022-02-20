9421c09c4a6546b9c3003a974efeb32858cefd6a
Bruno <bruno.lucas.rodriguez@gmail.com>
2022-02-20 17:57:34 -0300

    Create and get user

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,52 @@
-const createUser = (req, res,) => {
-	res.json({
-		testing: 123,
-	});
+const user = require("../services/user-services")
+
+/**
+ * Add a new user to the DB
+ */
+const createUser = (req, res, next) => {
+    console.log(req.body);
+
+    let username = req.body.username;
+
+    user.createUser(username, (err, data) =>{
+        if (err) {
+            return next(err);
+        }
+
+        if (!data) {
+            console.log("Missing `done()` argument");
+            return next({ message: "Missing callback argument" });
+        }
+
+        return res.json({
+            username: data.username,
+            _id: data.id
+        })
+    })
 };
 
+/**
+ * Get a list of all users (wip)
+ */
+const getUsers = (req,res, next) =>{
+
+    user.getUsers((err,data) =>{
+        if (err) {
+            return next(err);
+        }
+
+        if (!data) {
+            console.log("Missing `done()` argument");
+            return next({ message: "Missing callback argument" });
+        }
+
+        res.json(data);
+    })
+
+ 
+}
+
 module.exports = {
-    createUser
+    createUser,
+    getUsers
 }
