f01884ce43853f5a17b3b2f883ac3d398b21195f
Bruno <bruno.lucas.rodriguez@gmail.com>
2022-02-19 22:29:40 -0300

    added body parser

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require("body-parser");
+
 require('dotenv').config()
 
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
